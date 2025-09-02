77c2e994775d875b39827faed992c751a7ed177f
Nipun <nipunpilane@gmail.com>
2025-09-02 21:59:00 +0530

    Intial commit

--- Function & Array/Arrays.java
+++ Function & Array/Arrays.java
@@ -2,10 +2,7 @@ public class Arrays {
     public static void main(String args[]){
         //Defining an array in java
         //type[]arrayName =new type[size];
-
-
         // int marks[]={97,98,99};   This is also write statement not required line 8 To 12
-
         int[] marks =new int [3];
         marks[0]=97;
         marks[1]=98;
@@ -15,13 +12,7 @@ public class Arrays {
         // System.out.println(marks[2]);
         // System.out.println(marks);
         for(int i=0;i<3;i++){
-            System.out.println(marks[i]);
-            
+            System.out.println(marks[i]);    
         }
-
-
-        
-
-    }
-    
+    }   
 }
